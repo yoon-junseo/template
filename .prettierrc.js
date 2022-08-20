53a9609891e329e3f08d3a181641143de2340a1e
jun5e00 <ysj6844@naver.com>
2022-08-20 09:48:05 +0900

    chore: prettier
    1. deprecated jsxBracketSameLine -> bracketLine
    #20

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "printWidth": 120,
-  "tabWidth": 2,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "semi": true,
-  "useTabs": false,
-  "arrowParens": "always",
-  "endOfLine": "auto",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": true
-}
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  bracketLine: true,
+};
