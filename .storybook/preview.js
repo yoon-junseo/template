69de9064220a5e784e63a71d74671e98a48b22e6
jun5e00 <ysj6844@naver.com>
2022-08-20 09:47:31 +0900

    feat: storybook 설정
    1. preview Story 추가
    2. 절대 경로 추가
    #20

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from '@/styles/GlobalStyle';
+import { Theme } from '@/styles/Theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
