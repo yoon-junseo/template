5a1d0eca9b9e782e596a60e24f51e5d7667793ee
jun5e00 <ysj6844@naver.com>
2022-08-20 09:45:09 +0900

    chore: storybook lint 추가
    #20

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,58 +2,52 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2017,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['airbnb', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
   rules: {
     /** 함수의 명시적 타입 리턴을 명시적으로 써주지 않아도 되도록 하는 옵션. */
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     '@typescript-eslint/no-var-requires': 0,
-
     // prettier
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-
+    'prettier/prettier': ['error', {
+      endOfLine: 'auto'
+    }],
     'no-use-before-define': 'off',
     'import/no-unresolved': 'off',
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }],
     // label
     'jsx-a11y/label-has-associated-control': 'off',
-
     // import
     'import/extensions': 'off',
-
     // 단항 연산자 가능하게
     'no-plusplus': 'off',
-
     // single quote 가능하게
-    'dot-notation': [2, { allowKeywords: false }],
+    'dot-notation': [2, {
+      allowKeywords: false
+    }]
   },
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
     'import/resolver': {
       node: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
-      },
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
+      }
     },
-    'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
-  },
+    'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx']
+  }
 };
